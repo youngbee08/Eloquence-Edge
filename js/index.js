10cb988f0c5b704c4ec32436210330cea538c81c
youngbee08 <youngbeeh08@gmail.com>
2025-05-23 14:42:46 +0100

    updated the landing page

--- js/index.js
+++ js/index.js
@@ -1,16 +1,16 @@
-var typed = new Typed("#typewriter", {
-  strings: [
-    "Where we help you improve your speaking skills.",
-    "Where we help you master communication like a pro.",
-    "Where we help you speak with clarity and impact.",
-    "Where we help you express yourself fluently.",
-    "Where we help you build confidence through words."
-  ],
-  typeSpeed: 60,
-  backSpeed: 40,
-  backDelay: 2000,
-  loop: true
-});
+// var typed = new Typed("#typewriter", {
+//   strings: [
+//     "Where we help you improve your speaking skills.",
+//     "Where we help you master communication like a pro.",
+//     "Where we help you speak with clarity and impact.",
+//     "Where we help you express yourself fluently.",
+//     "Where we help you build confidence through words."
+//   ],
+//   typeSpeed: 60,
+//   backSpeed: 40,
+//   backDelay: 2000,
+//   loop: true
+// });
 // Event Listeners for buttons //
 const getStartedBtn = document.querySelector("#getStartedBtn");
 getStartedBtn.addEventListener("click", goTodashBoard)
